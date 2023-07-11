7b51adee3721e2907fd43c45c2434d8cbf25a9cc
Roman <mazaxaka.tyt@gmail.com>
2023-07-11 13:59:23 +0300

    add housing section

---
+++ app.js
@@ -0,0 +1,10 @@
+// housing slider
+
+$(document).ready(function () {
+    $(".housing-slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+    });
+});
